b58fc30d84b242e78921bf728edafa63cdea8ed6
alperentankut <tankut.alperen@gmail.com>
2022-01-24 00:31:03 +0300

    data type added

---
+++ level2/js/advanced/type.js
@@ -0,0 +1,38 @@
+// Primitive
+
+
+// var a = 10;
+// var b = 3.14;
+
+// console.log(typeof a); // typeof etiketi veri tipini yazdırır.
+// console.log(typeof b); //Number veri tipi
+
+// //String
+
+// var name = "Alperen";
+// console.log(typeof name);
+
+//boolean
+
+
+// var a = true;
+
+// console.log(typeof a);
+
+
+//null
+
+var a = null; //null a nın hiçbir değer taşımadığını vurgulamak için kullanılır. Ama typeof dediğimiz zaman object olarak karşımıza çıkıyor
+//bunun sebebi js deki bir bug
+console.log(typeof a);
+
+
+
+
+
+
+
+
+
+
+// Reference veri tipi
