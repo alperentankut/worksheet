cdab7f6b02bc0c095b80baf3bc114b1b3da27d86
alperentankut <tankut.alperen@gmail.com>
2022-01-26 22:44:08 +0300

    operators

---
+++ level2/js/advanced/js/operators.js
@@ -0,0 +1,25 @@
+let value ;
+
+
+//Basit matematik operatörleri
+
+
+value1 = 10;
+value2 = 2;
+
+// value = value1 + value2; //Toplama
+// value = value1 - value2; //Çıkarma
+// value = value1 * value2  //Çarpma
+// value = value1 / value2  //Bölme
+// value = value1 % value2  //1 in 2 ye bölümünden kalan
+
+value = Math.PI;
+value = Math.round(3.6); //round sayıyı yuvarlamaya yarayan operatördür.
+value = Math.ceil(3.2); //ceil operatörü bi üstteki sayıya yuvarlıyor.
+
+
+
+
+
+
+console.log(value);
