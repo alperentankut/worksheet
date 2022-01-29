402fe79cd5423d1a74ab0f63691380b8c8ebd4a1
alperentankut <tankut.alperen@gmail.com>
2022-01-29 11:20:15 +0300

    operators added

--- level2/js/advanced/js/operators.js
+++ level2/js/advanced/js/operators.js
@@ -16,6 +16,15 @@ value2 = 2;
 value = Math.PI;
 value = Math.round(3.6); //round sayıyı yuvarlamaya yarayan operatördür.
 value = Math.ceil(3.2); //ceil operatörü bi üstteki sayıya yuvarlıyor.
+value = Math.floor(3.6); //floor operatörü bir alttaki sayıya yuvarlıyor.
+value = Math.sqrt(16); //karekök.
+value = Math.abs(-10); //mutlak değer.
+value = Math.pow(3,4); //üs alma 3 ün 4. kuvveti.
+value = Math.max(2,-5,8,2); //En büyük değeri veriyor.
+value = Math.min(2,5,3,8,33); //en küçüğü.
+value = Math.random(); //0-1 arasında rastgele sayı.
+value = Math.random() * 20 ; //0-20 arasında değer atıyor.
+value = Math.floor(Math.random() * 20 + 1); //tam sayı değer için yuvarlama metodunu kullandık "+1" 20 dahil için kullanılıyor.
 
 
 
