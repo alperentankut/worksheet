cf9d6619de1d3e7eb4e21c6d825cbc0b2ec0c7f2
alperentankut <tankut.alperen@gmail.com>
2022-01-30 21:53:49 +0300

    switchCase added

---
+++ level2/js/advanced/js/switchCase.js
@@ -0,0 +1,21 @@
+const process = 2;
+
+switch (process){
+    case 1 : 
+        console.log("İşlem 1");
+        break; //switch case den çıkmak için yazmamız gerekiyor. Break kullanmazsak diğer case ler şartı sağlamasa bile çalışacaktır.
+
+    case 2 :
+        console.log("İşlem 2");
+        break;
+    case 3 :
+        console.log("İşlem 3");
+        break;
+
+    default : 
+        console.log("Geçersiz işlem");
+    
+
+
+
+}
