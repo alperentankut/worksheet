ae569cc8dfeddaf3e63c4c7c1878d3723007b46f
alperentankut <tankut.alperen@gmail.com>
2022-02-28 20:40:55 +0300

    React added

---
+++ Javascript/React/onevideoreact/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const title = "User App";
+
+  return (
+   <div className = "container">
+     <h5>{title}</h5>
+     
+     <p className = "lead"> this is sample text</p>
+   </div>
+  );
+}
+
+export default App;
