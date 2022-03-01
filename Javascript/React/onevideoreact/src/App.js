7e160eb10851b98bb0ed36074819c64cd2208a66
alperentankut <tankut.alperen@gmail.com>
2022-03-01 09:53:21 +0300

    React updated

--- Javascript/React/onevideoreact/src/App.js
+++ Javascript/React/onevideoreact/src/App.js
@@ -1,15 +1,51 @@
+import React, { Component } from "react";
+import AddUser from "./components/AddUser";
+import Users from "./components/Users";
 
 
-function App() {
-  const title = "User App";
 
-  return (
-   <div className = "container">
-     <h5>{title}</h5>
-     
-     <p className = "lead"> this is sample text</p>
-   </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [
+        {
+          id : 1,
+          name : "Alperen Tankut",
+          email : "tankut.alperen@gmail.com"
+        },
+        {
+          id : 2,
+          name : "Alperen",
+          email : "tankut.alperen1@gmail.com"
+        },
+        {
+          id : 3,
+          name : "Tankut",
+          email : "tankut.alperen2@gmail.com"
+        },
+      ]
+    };
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <h4>User app</h4>
+        <hr />
+        <AddUser />
+        <hr/>
+        <Users />
+      </div>
+    )
+  }
+    
+      
+
+
+  
 }
 
 export default App;
