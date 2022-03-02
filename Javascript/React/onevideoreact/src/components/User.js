398abd8bbf8d7dd405c5b885e7864d15d4504312
alperentankut <tankut.alperen@gmail.com>
2022-03-02 16:07:11 +0300

    React updated

--- Javascript/React/onevideoreact/src/components/User.js
+++ Javascript/React/onevideoreact/src/components/User.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 
 
 class User extends Component{
+    // constructor(props) {
+    //     super(props);
 
+    //     this.onDeleteClick = this.onDeleteClick.bind(this);
+    // } bu bir yöntem. Diğeri de onclick eventinin içine yazmak. Constructor daha iyi gibi.
+    // event her zaman en son yazılır.
 
+        onDeleteClick(id,event) {
+         console.log(this);   
+        }
     render(){
         const {id, name, email} = this.props;
 
@@ -13,7 +21,7 @@ class User extends Component{
                 <td>{id}</td>
                 <td>{name}</td>
                 <td>{email}</td>
-                <td><button className="btn btn-danger">Delete</button></td>
+                <td><button onClick={this.onDeleteClick.bind(this,id)} className="btn btn-danger">Delete</button></td>
                 
             </tr>
         )
