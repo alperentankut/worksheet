1ca1a2fceaf6433d395b2984b41e3600be3b1d43
alperentankut <tankut.alperen@gmail.com>
2022-03-10 22:09:13 +0300

    constructor update

--- Javascript/js-primary/OOP-Prototype/Constructor and create object/app.js
+++ Javascript/js-primary/OOP-Prototype/Constructor and create object/app.js
@@ -1,15 +1,44 @@
 
-const emp1 = { //Object Literal
-    name : "Alperen",
-    age : 25,
-    showInfos : function() {console.log("Bilgiler Gösteriliyor..");} 
-};
-const emp2 = {
-    name : "Oguzcan",
-    age : 25
-};
+// const emp1 = { //Object Literal
+//     name : "Alperen",
+//     age : 25,
+//     showInfos : function() {console.log("Bilgiler Gösteriliyor..");} 
+// };
+// const emp2 = {
+//     name : "Oguzcan",
+//     age : 25
+// };
 
-emp1.salary = 4500 //yeni bir değer eklemek için kullanıyoruz.
-emp1.showInfos();
+// emp1.salary = 4500 //yeni bir değer eklemek için kullanıyoruz.
+// emp1.showInfos();
 
-console.log(emp1);
+// console.log(emp1);
+
+
+
+// function Employee(){ //Constructor (Yapıcı) fonskiyon 
+//     this.name = "Alperen";
+// }
+
+//Örneğin biz daha önce new Date() fonksiyonuyla date objesini alabiliyorduk
+//aslında bu new Date js geliştiricileri tarafından yazılmış hazır bir obje 
+//Bizde yukarda Employee şeklinde kendimiz bir obje oluşturduk. Bunu kullanırken
+//new Employee şeklinde kullanıyoruz. Ancak yukardaki gibi yazmak pek de mantıklı
+//değil. Çünkü bu constructor'ı kullandığımızda bütün name özelliklerimiz alperen
+//olacak
+
+function Employee(name,age,salary){
+    this.name = name,
+    this.age = age,
+    this.salary = salary;
+
+    console.log(this) //burdaki this Employee yi verecek. Global scope
+    //da kullansaydık window objesini verecekti
+}
+
+const emp1 = new Employee("Alperen" , 25 , 4500);
+const emp2 = new Employee("Oguz" , 25 , 20000);
+
+ 
+
+
