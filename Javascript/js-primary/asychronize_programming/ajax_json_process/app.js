b2c0ab2a138d058cb68c35fabec984583e73384f
alperentankut <tankut.alperen@gmail.com>
2022-03-30 09:34:26 +0300

    ajax and JSON added

---
+++ Javascript/js-primary/asychronize_programming/ajax_json_process/app.js
@@ -0,0 +1,44 @@
+document.getElementById("ajax").addEventListener("click" , getAllEmployees)
+
+function getAllEmployees(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET","employees.json",true);
+
+    xhr.onload = function(){
+        let list = document.getElementById("employees");
+        
+        if(this.status == 200){
+            const employees = JSON.parse(this.responseText)
+
+            employees.forEach(function(employee){
+                list.innerHTML += `
+                
+                <tr>
+                    <td>${employee.name}</td>
+                    <td>${employee.department}</td>
+                    <td>${employee.salary}</td>
+                </tr>
+                `;
+                
+            }
+            
+        )};
+    }
+
+
+    xhr.send();
+
+
+}
+
+
+//open fonksiyonuyla bağlantıyı kuruyoruz. Sonrasında get komutuyla 
+//JSON dosyamızdan verileri almak istediğimizi söylüyoruz. Sonrasında
+//verileri alacağımız dosya adını yazıyoruz ve en son true diyerek 
+//bu işlemin asenkron olmasını istediğimiz söylüyoruz.
+//sonrasında send fonksiyonu ile isteğimizi gönderiyoruz.
+
+//Bizim response 'umuz geldiğince onload çalışmış olacak.
+//Bize gelen değer string oluyor. Biz bunu JSON.parse ile bir array e 
+//dönüştürüyoruz.
