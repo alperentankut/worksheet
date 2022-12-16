d84b54bf8f987b90b665cfa0a4afced458d05626
alperentankut <tankut.alperen@gmail.com>
2022-12-16 15:07:27 +0300

    draft update

--- draft/app.js
+++ draft/app.js
@@ -1,13 +1,32 @@
 var rob = function(nums){
-    let toplam = 0;
-    for(let i = 0 ; i<nums.length ; i++){
-        if(i%2 == 0){
-            toplam = nums[i] + toplam
-            
-        }
+    let total = 0;
+    let secondTotal = 0;
+    if(nums.length == 1){
+        total = nums[0]
+        return total
+    }   
+    else if(nums.length == 2){
+        total = Math.max(nums[0] , nums[1])
+        return total
     }
-    return toplam
-    
+    else{
+        for(let i = 0 ; i<nums.length ; i++){
+            if(i%2 == 0){
+                total =nums[i] + total;
+            }
+            else if(i%2 == 1){
+                secondTotal = nums[i]+secondTotal; 
+            }
+        }
+        if(secondTotal>total){
+            return secondTotal
+        }
+        else{
+            return total
+        }
+        
+        
+    }
 }
 
-console.log(rob([1,3,5,7]))
+console.log(rob([1,3,1]))
