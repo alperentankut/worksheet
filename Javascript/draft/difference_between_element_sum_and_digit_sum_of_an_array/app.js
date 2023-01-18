d018a4e65239cb5fa8975e0ff1594f046542763e
alperentankut <tankut.alperen@gmail.com>
2023-01-18 14:51:27 +0300

    difference of sum solution

--- Javascript/draft/difference_between_element_sum_and_digit_sum_of_an_array/app.js
+++ Javascript/draft/difference_between_element_sum_and_digit_sum_of_an_array/app.js
@@ -1,14 +1,18 @@
-let countNum = 0
-let finalNums = 0
-let countNums;
 var differenceOfSum =function(nums){
-    
+    let firstCount = 0
+    let secondNums = 0
     for(i = 0 ; i<nums.length ; i++){
-        countNum = countNum + nums[i];
-        
+        firstCount = firstCount + nums[i];
+        if(nums[i]%10 == nums[i]){
+            secondNums += nums[i]
+        }
+        else {
+            secondNums += nums[i]%10
+            secondNums += Math.floor(nums[i]/10)%10
+            secondNums += Math.floor(nums[i]/100)%10
+            secondNums += Math.floor(nums[i]/1000)%10
+        }
     }
-    
+    return (firstCount - secondNums)  
 }
-
-console.log(differenceOfSum([1,15,6,3]))
-console.log(countNums)
+differenceOfSum([1,15,6,3])
