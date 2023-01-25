e090d1989d7adba81cb24a9a6799abb32ef90f6e
alperentankut <tankut.alperen@gmail.com>
2023-01-25 12:01:14 +0300

    roman to integer added

---
+++ Javascript/draft/roman_to_integer/app.js
@@ -0,0 +1,14 @@
+var romanToInt = function(){
+    const roman = {
+        "I" : 1 , 
+        "V" : 5 ,
+        "X" : 10 ,
+        "L" : 50 , 
+        "C" : 100 , 
+        "D" : 500 , 
+        "M" : 1000
+    };
+}
+
+
+console.log()
--- Javascript/draft/two_sum/app.js
+++ Javascript/draft/two_sum/app.js
@@ -1,20 +1,11 @@
 var twoSum = function(nums , target){
-    let result =[]
-    for(let i=1 ; i<nums.length ; i++){
-        for(var el of nums) {
-            if(i !== nums.indexOf(el)){
-                if(nums[i] + el == target)
-                result.push(i,nums.indexOf(el))
-                result.splice(2)
-            }
-            
-        };
-    }
-   
-    return result
+   for(i = 0 ; i<nums.length ; i++){
+    const reduceNum =function(acc , currentValue)
+        {acc + currentValue}
+   }
+   console.log(nums.reduce(reduceNum(nums[i],nums)))
 }
 console.log(twoSum([2,6,4,2,88,6,55] , 90))
-
 
 
 
