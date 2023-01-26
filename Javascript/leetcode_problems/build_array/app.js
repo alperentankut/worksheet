d30c2acc0b2466039df079b8fe5a6f87809eef71
alperentankut <tankut.alperen@gmail.com>
2023-01-26 22:29:28 +0300

    build array solution added

---
+++ Javascript/leetcode_problems/build_array/app.js
@@ -0,0 +1,11 @@
+var buildArray = function(nums){
+    let newArr = [];
+    for(i = 0 ; i<nums.length ; i++){
+        newArr.push(nums[nums[i]])
+    }
+    return newArr
+}
+
+
+console.log(buildArray([0,2,1,5,3,4]))
+
