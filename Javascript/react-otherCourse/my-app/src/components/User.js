d23fc7ab32119ca4abc72e723872996c1bef13df
alperentankut <tankut.alperen@gmail.com>
2023-03-22 09:30:15 +0300

    state added

--- Javascript/react-otherCourse/my-app/src/components/User.js
+++ Javascript/react-otherCourse/my-app/src/components/User.js
@@ -2,22 +2,39 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class User extends Component {
+  state = {
+    isVisible : false
+  } //bu da state oluşturmanın ikinci yolu
+  
+  
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     isVisible: false,
+  //   };
+  
   render() {
     // Destructing
 
     const { name, department, salary } = this.props;
-
+    const { isVisible } = this.state;
     return (
       <div className="col-md-8 mb-4">
         <div className="card">
           <div className="card-header d-flex justify-content-between">
             <h4 className="d-inline">{name}</h4>
-            <i className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
+            <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
           </div>
-          <div className="card-body">
+          {
+            isVisible ? <div className="card-body">
             <p className="card-text">Maaş : {salary}</p>
             <p className="card-text">Departman : {department}</p>
-          </div>
+          </div> : null
+          //Yukarda isVisible true ise card body i göster false ise
+          //gösterme şeklinde bir kod yazdık. Değeri başlangıçta false
+          //olarak verdiğimiz için gözükmeyecektir.
+          }
         </div>
       </div>
     );
@@ -39,3 +56,7 @@ export default User;
 
 //Biz this.props.name yerine destructing ile bundan kurtulup daha
 //kolay bir şekilde propslarımızı yazabiliriz
+
+// -- State --
+//state vermenin iki yolu vardır. Bir tanesi constructor'ın içinde vermek
+//diğeri de class ın en tepesinde oluşturmak.
