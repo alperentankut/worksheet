3df3202d03b5df2e51f20c32c1f10f322f8cde73
alperentankut <tankut.alperen@gmail.com>
2023-03-22 16:31:46 +0300

    add bind

--- Javascript/react-otherCourse/my-app/src/components/User.js
+++ Javascript/react-otherCourse/my-app/src/components/User.js
@@ -3,17 +3,24 @@ import PropTypes from "prop-types";
 
 class User extends Component {
   state = {
-    isVisible : false
-  } //bu da state oluşturmanın ikinci yolu
-  
-  
+    isVisible: false,
+  }; //bu da state oluşturmanın ikinci yolu
+
   // constructor(props) {
   //   super(props);
 
   //   this.state = {
   //     isVisible: false,
   //   };
-  
+  constructor(props){
+    super(props)
+    this.onClickEvent = this.onClickEvent.bind(this)
+  }
+
+  onClickEvent = (number,e) =>{
+    console.log(number)
+  }
+
   render() {
     // Destructing
 
@@ -23,17 +30,21 @@ class User extends Component {
       <div className="col-md-8 mb-4">
         <div className="card">
           <div className="card-header d-flex justify-content-between">
-            <h4 className="d-inline">{name}</h4>
+            <h4 className="d-inline" onClick={this.onClickEvent.bind(this,34)}>
+              {name}
+            </h4>
             <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
           </div>
           {
-            isVisible ? <div className="card-body">
-            <p className="card-text">Maaş : {salary}</p>
-            <p className="card-text">Departman : {department}</p>
-          </div> : null
-          //Yukarda isVisible true ise card body i göster false ise
-          //gösterme şeklinde bir kod yazdık. Değeri başlangıçta false
-          //olarak verdiğimiz için gözükmeyecektir.
+            isVisible ? (
+              <div className="card-body">
+                <p className="card-text">Maaş : {salary}</p>
+                <p className="card-text">Departman : {department}</p>
+              </div>
+            ) : null
+            //Yukarda isVisible true ise card body i göster false ise
+            //gösterme şeklinde bir kod yazdık. Değeri başlangıçta false
+            //olarak verdiğimiz için gözükmeyecektir.
           }
         </div>
       </div>
@@ -60,3 +71,21 @@ export default User;
 // -- State --
 //state vermenin iki yolu vardır. Bir tanesi constructor'ın içinde vermek
 //diğeri de class ın en tepesinde oluşturmak.
+
+
+//--Onclick event içindeki durum--
+//Bizim reactta render fonksiyonumuz componentten miras aldığımız bir 
+//fonksiyon. Yani bu componentin prototype ında bulunan bir metod.  
+//Bundan dolayı biz render fonksiyonumuzun içinde this dediğimizde user
+//objemizi gösterecektir ve bind etmemize gerek kalmayacak. 
+//Ama bizim kendi yazdığımız metodları bind etmemiz gerekecek. Çünkü
+//kendi yazdığımız metodları miras alacağımız bir prototype yok. Bunu
+//kendimiz yazmamız gerekecek. Bu bind işlemini constructor oluşturarak veya
+//yazdığımız metoda .bind(this) şeklinde ekleyebiliriz. Ama biz 
+//bunu arrow function kullanarak yazarsak daha temiz bir yol olur. Çünkü
+//arrow functionlar bind işlemini otomatik olarak yapıyor. 
+
+//Onclickte parametre gönderirken yine bind kullanmamız lazım. Burda
+//ilk parametremiz this olmalı ikinci parametre ise göndereceğimiz veri.
+//fonksiyonu çalıştırırkende önce parametremizi sonra eventimizi (e) 
+//çalıştırmamız gerekiyor
