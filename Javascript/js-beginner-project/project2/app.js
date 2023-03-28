e2c6d8ef6d20beb0c0f36dab930958d2ba6e8a87
alperentankut <tankut.alperen@gmail.com>
2023-03-28 12:54:27 +0300

    islamic prayer project finished

--- Javascript/js-beginner-project/project2/app.js
+++ Javascript/js-beginner-project/project2/app.js
@@ -13,18 +13,24 @@ function startTime() {
 }
 function checkTime(i) {
   if (i < 10) {
-    i = "0" + i
+    i = "0" + i;
   }
   return i;
 }
 
 //Button Event Listeners
-let button = document.getElementsByClassName("btn")
-let firstCity = document.getElementById("city1") //İstanbul
-let secondCity = document.getElementById("city2") //Ankara
-let thirdCity = document.getElementById("city3")  //İzmir
-let fourthCity = document.getElementById("city4")  //Gaziantep
+let button = document.querySelectorAll(".btn");
+let firstCity = document.getElementById("city1"); //İstanbul
+let secondCity = document.getElementById("city2"); //Ankara
+let thirdCity = document.getElementById("city3"); //İzmir
+let fourthCity = document.getElementById("city4"); //Gaziantep
 
-function addTime (city){
-    
+function addTime(city, location, time) {
+  city.addEventListener("click", () => {
+    location.innerHTML += "İftar saati : " + time;
+  });
 }
+addTime(firstCity, document.getElementById("istanbul"), 19.31);
+addTime(secondCity, document.getElementById("ankara"), 19.14);
+addTime(thirdCity, document.getElementById("izmir"), 19.37);
+addTime(fourthCity, document.getElementById("gaziantep"), 18.56);
