da09ae733990a7897db2f96cc5aed8753d81e06b
alperentankut <tankut.alperen@gmail.com>
2023-09-07 08:22:48 +0300

    import bcrypt and hash password

--- Javascript/js-beginner-project/question-answer-api-tutorial/models/User.js
+++ Javascript/js-beginner-project/question-answer-api-tutorial/models/User.js
@@ -1,74 +1,86 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
-    name : {
-        type : String,
-        required : [true , "Please provide a name"],
-    },
-    email : {
-        type : String,
-        required : [true,"Please provide a email"],
-        unique : [true,"Please try different email"],
-        match : [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Please provide a valid email"
-        ]
-    },
-    role : {
-        type : String,
-        default : "user",
-        enum : ["user","admin"]
-    },
-    password : {
-        type : String,
-        minlength : [6,"Please provide a password with min length 6"],
-        required : [true,"Please provide a password"],
-        select : false
-    },
-    createdAt : {
-        type : Date,
-        default : Date.now
-    },
-    title : {
-        type : String
-    },
-    about : {
-        type : String
-    },
-    place : {
-        type : String
-    },
-    website : {
-        type : String
-    },
-    profile_image : {
-        type : String,
-        default : "default.jpg"
-    },
-    blocked : {
-        type : Boolean,
-        default : false
-    }
-
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: [true, "Please try different email"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please provide a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "admin"],
+  },
+  password: {
+    type: String,
+    minlength: [6, "Please provide a password with min length 6"],
+    required: [true, "Please provide a password"],
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  title: {
+    type: String,
+  },
+  about: {
+    type: String,
+  },
+  place: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  profile_image: {
+    type: String,
+    default: "default.jpg",
+  },
+  blocked: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-UserSchema.pre("save",function(next){
-    console.log("Pre hooks : Save");
-    next()
-})
+UserSchema.pre("save", function (next) {
+  //if password not change
+  if (!this.isModified("password")) {
+    next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) next(err);
+      this.password = hash;
+      next();
+    });
+  });
+});
 
 //Yukarda yazdığımız kod mongoose ile gelen bir
 //hook özelliği. Burda pre fonksiyonuyla save
-//işleminden hemen sonra yapmak istediğimiz 
+//işleminden hemen sonra yapmak istediğimiz
 //fonksiyonu yazıyoruz. Fonksiyonun ilk parametresi
-//hangi işlemden önce yapılmasını istiyorsak o 
+//hangi işlemden önce yapılmasını istiyorsak o
 //işlemi alıyor ikinci parametre ise yazmak istediğimiz
 //fonksiyonu.
+//Biz parolamız değişmemişse fonksiyonun boşa çalışmaması için
+//mongoose içerisinde bulunan isModified fonksiyonunu kullanabiliriz
+//Bu fonksiyonda içine girdiğimiz değer değişmişse true
+//değişmemişse false gönderiyor.
 
-module.exports  = mongoose.model("User" , UserSchema)
+module.exports = mongoose.model("User", UserSchema);
 
 //Yukardaki export ile oluşturduğumuz UserSchema yı User ismiyle
 //mongoose a kaydediyoruz.
@@ -77,11 +89,10 @@ module.exports  = mongoose.model("User" , UserSchema)
 //kişi örneğin alperen.com diye bir mail girdiğinde bunun geçersiz
 //olduğunu ve gerçek mail formatında yazması gerektiğini söylüyoruz.
 
-
-//role kısmında kullanıcının admin mi yoksa standart kullanıcı mı 
+//role kısmında kullanıcının admin mi yoksa standart kullanıcı mı
 //olduğunu belirledik. Kullanıcı tiplerimizi enum içersinide tanımlıyoruz
 //Bizim iki tip kullanıcımız olduğu için user ve admin olarak tanımladık
 
-//Biz ilerde getAllUser fonksiyouyla bütün kullanıcı bilgilerini 
+//Biz ilerde getAllUser fonksiyouyla bütün kullanıcı bilgilerini
 //çekeceğimiz zaman password bilgisini götermememiz lazım. Bunun için
 //select : false şeklinde bir komut ekledik.
